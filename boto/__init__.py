6a21f9b7731dd3c67a7b766f7c632062cea972f7
nick <nicholas@listingmirror.com>
2019-11-18 16:07:02 -0500

    update-mws-req-post-body allowing amazon api calls to send through req body and not just params

--- boto/__init__.py
+++ boto/__init__.py
@@ -38,7 +38,7 @@ import logging.config
 from boto.compat import urlparse
 from boto.exception import InvalidUriError
 
-__version__ = '2.40.0.2'
+__version__ = '2.40.0.3'
 Version = __version__  # for backware compatibility
 
 # http://bugs.python.org/issue7980
